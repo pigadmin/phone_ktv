9301119fb1d310ef3ed64a3b1848a9096408c0db
635488410@qq.com <635488410@qq.com>
2018-07-18 14:31:46 +0800

    ..

--- app/src/main/java/phone/ktv/activitys/RegisterActivity.java
+++ app/src/main/java/phone/ktv/activitys/RegisterActivity.java
@@ -1,7 +1,11 @@
 package phone.ktv.activitys;
 
+import android.content.Context;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 import phone.ktv.R;
 
@@ -10,9 +14,36 @@ import phone.ktv.R;
  */
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "RegisterActivity";
+
+    Context mContext;
+
+    private TextView mRegistProtocol;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity_layout);
+
+        mContext=RegisterActivity.this;
+        initView();
+        initLiter();
+    }
+
+    private void initView(){
+        mRegistProtocol=findViewById(R.id.regist_protocol_tvw);
+        mRegistProtocol.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);//下划线
+    }
+
+    private void initLiter(){
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
