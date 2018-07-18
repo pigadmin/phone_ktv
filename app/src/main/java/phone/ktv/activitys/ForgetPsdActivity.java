c4d3629ee09434f9da0e23ea0069e33c6c98f072
635488410@qq.com <635488410@qq.com>
2018-07-18 13:30:34 +0800

    ..

--- app/src/main/java/phone/ktv/activitys/ForgetPsdActivity.java
+++ app/src/main/java/phone/ktv/activitys/ForgetPsdActivity.java
@@ -19,12 +19,7 @@ public class ForgetPsdActivity extends AppCompatActivity implements View.OnClick
     private static final String TAG = "ForgetPsdActivity";
     private Context mContext;
 
-    private EditText mInputPhone;//手机号
-    private EditText mInputPsd;//密码
-    private CheckBox mJizhuPsd;//记住密码
-    private TextView mWangjiPsd;//忘记密码
-    private TextView mLogin;//登陆
-    private TextView mReginster;//注册
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,31 +32,19 @@ public class ForgetPsdActivity extends AppCompatActivity implements View.OnClick
     private void initView() {
         mContext = ForgetPsdActivity.this;
 
-        mInputPhone=findViewById(R.id.phone_edt);
-        mInputPsd=findViewById(R.id.password_edt);
-        mJizhuPsd=findViewById(R.id.jizhu_ckb);
-        mWangjiPsd=findViewById(R.id.wangji_tvw);
-        mLogin=findViewById(R.id.login_tvw);
-        mReginster=findViewById(R.id.register_tvw);
+
     }
 
     private void initLiter(){
-        mWangjiPsd.setOnClickListener(this);
-        mLogin.setOnClickListener(this);
-        mReginster.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.wangji_tvw:
-                break;
+//            case R.id.wangji_tvw:
+//                break;
 
-            case R.id.login_tvw:
-                break;
-
-            case R.id.register_tvw:
-                break;
         }
     }
 
