c9495a408a80c9deef013964e311cbbe5b9fce5a
635488410@qq.com <635488410@qq.com>
2018-07-24 15:22:01 +0800

    ..

--- app/src/main/java/phone/ktv/activitys/already_activitys/AlreadySearchActivity.java
+++ app/src/main/java/phone/ktv/activitys/already_activitys/AlreadySearchActivity.java
@@ -1,19 +1,24 @@
 package phone.ktv.activitys.already_activitys;
 
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import phone.ktv.R;
-import phone.ktv.tootls.Logger;
+import phone.ktv.adaters.RinkingListAdater;
+import phone.ktv.bean.MusicPlayBean;
 import phone.ktv.tootls.ScreenUtils;
 
 /**
@@ -25,9 +30,16 @@ public class AlreadySearchActivity extends AppCompatActivity {
 
     Context mContext;
 
-    private ImageView mSrcBack11;
+    private ImageView mSrcBack11;//返回
+    private ImageView mVoice;//语音功能
+    private TextView mSongType;//歌名/歌星
+    private EditText mSearchContent;//搜索内容=
+    private TextView mSearch;//搜索按钮
 
-    private TextView mSongType;
+    private ListView mListView;
+    private RinkingListAdater mRinkingAdater;
+
+    private List<MusicPlayBean> musicPlayBeans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +51,49 @@ public class AlreadySearchActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        musicPlayBeans=new ArrayList<>();
+
         mContext = AlreadySearchActivity.this;
 
         mSrcBack11 = findViewById(R.id.src_back11_ivw);
         mSongType = findViewById(R.id.songType_tvw11);
+        mVoice= findViewById(R.id.voice12_ivw);
+        mSearchContent= findViewById(R.id.search_content_edt);
+        mSearch= findViewById(R.id.text_search11_tvw);
+        mListView = findViewById(R.id.list112_view);
+
+        musicPlayBeans.add(new MusicPlayBean());
+
+
+        mRinkingAdater=new RinkingListAdater(mContext,R.layout.item_ringlist_layout,musicPlayBeans);
+        mListView.setAdapter(mRinkingAdater);
     }
 
     private void initLiter() {
         mSrcBack11.setOnClickListener(new MyOnClickBackReturn());//返回事件
         mSongType.setOnClickListener(new MyOnClickSongType());
+        mVoice.setOnClickListener(new MyOnClickListenerVoice());
+        mSearch.setOnClickListener(new MyOnClickListenerSearch());
+    }
+
+    /**
+     * 搜索事件
+     */
+    private class MyOnClickListenerSearch implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    /**
+     * 语音
+     */
+    private class MyOnClickListenerVoice implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 
     /**
@@ -61,7 +107,7 @@ public class AlreadySearchActivity extends AppCompatActivity {
     }
 
     /**
-     * 返回事件
+     * 显示选择框
      */
     public class MyOnClickSongType implements View.OnClickListener {
         @Override
