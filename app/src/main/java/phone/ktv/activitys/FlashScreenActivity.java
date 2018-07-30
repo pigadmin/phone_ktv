df4964950787421f8cad507a6149eb0b38feedfe
zhu <964182377@qq.com>
2018-07-30 15:29:20 +0800

    2

--- app/src/main/java/phone/ktv/activitys/FlashScreenActivity.java
+++ app/src/main/java/phone/ktv/activitys/FlashScreenActivity.java
@@ -2,14 +2,22 @@ package phone.ktv.activitys;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,6 +31,7 @@ import phone.ktv.R;
 import phone.ktv.app.App;
 import phone.ktv.bean.AJson;
 import phone.ktv.bean.AdverOpenOne;
+import phone.ktv.tootls.FULL;
 import phone.ktv.tootls.IntentUtils;
 import phone.ktv.tootls.Logger;
 import phone.ktv.tootls.NetUtils;
@@ -33,7 +42,7 @@ import phone.ktv.tootls.ToastUtils;
 /**
  * 闪屏页面
  */
-public class FlashScreenActivity extends Activity implements View.OnClickListener {
+public class FlashScreenActivity extends Activity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
     private static final String TAG = "FlashScreenActivity";
 
     private TextView mNumtext;
@@ -57,6 +66,10 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
     }
 
 
+    private ImageView ad_image;
+    private VideoView ad_video;
+    MediaPlayer mediaPlayer;
+
     private void initView() {
         mOpenTwoList = new ArrayList<>();
 
@@ -68,7 +81,24 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
         mNumtext = findViewById(R.id.num);
         mNumtext.setOnClickListener(this);
 
-        startTime();
+        ad_image = findViewById(R.id.ad_image);
+        ad_video = findViewById(R.id.ad_video);
+        FULL.star(ad_video);
+        ad_video.setOnPreparedListener(this);
+        ad_video.setOnErrorListener(this);
+        ad_video.setOnCompletionListener(this);
+
+        mediaPlayer = new MediaPlayer();
+
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playmusic();
+            }
+        });
+//        startTime();
     }
 
     @Override
@@ -85,11 +115,21 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        timer.cancel();
+        if (timer != null) {
+            timer.cancel();
+        }
+        exitmusic();
+    }
+
+    private void exitmusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
     }
 
     private void startTime() {
-        timer = new CountDownTimer(5000, 1000) {
+        timer = new CountDownTimer(playTime, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 mNumtext.setText("倒计时(" + millisUntilFinished / 1000 + ")秒");
@@ -119,6 +159,9 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
     /**
      * 获取开机动画
      */
+    int playTime = 0;
+    List<AdverOpenOne> openOne;
+
     private void submData() {
         String url = App.headurl + "open/ad";
         Logger.i(TAG, "url.." + url);
@@ -140,13 +183,23 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
                     if (aJson != null) {
                         if (aJson.getCode() == 0) {
                             Logger.i(TAG, "aJson1.." + aJson.toString());
-                            List<AdverOpenOne> openOne = aJson.getData();
+                            openOne = aJson.getData();
                             if (openOne != null) {
                                 for (AdverOpenOne one : openOne) {
                                     if (one.ad != null) {
                                         mOpenTwoList.add(one.ad);
+                                        playTime += one.playTime;
+
                                     }
                                 }
+                                handler.sendEmptyMessage(UPDATE_AD);
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        playTime *= 1000;
+                                        startTime();
+                                    }
+                                });
                             } else {
 
                             }
@@ -154,6 +207,14 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
 //                            mHandler.sendEmptyMessage(UpdateRequestSuccess);
                         } else {
 //                            mHandler.obtainMessage(UpdateRequestError, aJson.getMsg()).sendToTarget();
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    playTime = 5000;
+                                    startTime();
+                                }
+                            });
+
                         }
                     }
 
@@ -166,5 +227,90 @@ public class FlashScreenActivity extends Activity implements View.OnClickListene
 //            mSvProgressHUD.dismiss();
             ToastUtils.showShortToast(mContext, "网络连接异常,请检查网络配置");
         }
+
+
+    }
+
+    private final int UPDATE_AD = 0;
+    private int currentad = 0;
+    private AdverOpenOne.AdverOpenTwo ad;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case UPDATE_AD:
+                    if (currentad < openOne.size()) {
+                        ad = mOpenTwoList.get(currentad);
+                        switch (ad.type) {
+                            case 1:
+                                if (ad_image.isShown()) {
+                                    ad_image.setVisibility(View.GONE);
+                                }
+                                if (!ad_video.isShown()) {
+                                    ad_video.setVisibility(View.VISIBLE);
+                                }
+                                videourl = ad.path;
+                                if (mediaPlayer.isPlaying()) {
+                                    mediaPlayer.stop();
+                                    mediaPlayer.reset();
+                                }
+                                playvideo();
+                                break;
+                            case 2:
+                                if (!ad_image.isShown()) {
+                                    ad_image.setVisibility(View.VISIBLE);
+                                }
+                                if (ad_video.isShown()) {
+                                    ad_video.setVisibility(View.GONE);
+                                }
+                                videourl = ad.backFile;
+                                Picasso.with(FlashScreenActivity.this).load(ad.path).into(ad_image);
+                                playmusic();
+                                break;
+                        }
+                        handler.sendEmptyMessageDelayed(UPDATE_AD, openOne.get(currentad).playTime * 1000);
+                        currentad++;
+                    }
+                    break;
+            }
+        }
+    };
+    private String videourl;
+
+    private void playvideo() {
+
+        if (!videourl.equals("")) {
+            System.out.println("___________________________");
+            System.out.println(videourl);
+            ad_video.setVideoURI(Uri.parse(videourl));
+        }
+    }
+
+    private void playmusic() {
+        try {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(FlashScreenActivity.this,
+                    Uri.parse(videourl));
+            mediaPlayer.prepareAsync();
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return true;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        System.out.println("=================");
+        mediaPlayer.start();
     }
 }
---
+++ app/src/main/java/phone/ktv/service/MyService.java
@@ -0,0 +1,99 @@
+package phone.ktv.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.LinearLayout;
+
+import phone.ktv.R;
+import phone.ktv.tootls.DensityUtil;
+
+public class MyService extends Service {
+    String TAG = "MyService";
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createFloatView();
+    }
+
+    private LayoutParams wmParams;
+    private WindowManager mWindowManager;
+    private int screenWidth;
+    private int screenHeight;
+    private LinearLayout mFloatLayout;
+
+    private void createFloatView() {
+        try {
+            wmParams = new WindowManager.LayoutParams();
+
+            // 获取WindowManagerImpl.CompatModeWrapper
+            mWindowManager = (WindowManager) getApplication().getSystemService(
+                    getApplication().WINDOW_SERVICE);
+
+
+            DisplayMetrics dm = new DisplayMetrics();
+            mWindowManager.getDefaultDisplay().getMetrics(dm);
+            screenWidth = dm.widthPixels;//宽度height = dm.heightPixels ;//高度
+            screenHeight = dm.heightPixels;
+            Log.i(TAG, screenWidth + "-->" + screenHeight);
+
+            // 设置window type
+            wmParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+            // 设置图片格式，效果为背景透明
+            wmParams.format = PixelFormat.RGBA_8888;
+            // 设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
+            wmParams.flags =
+                    // LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+            // LayoutParams.FLAG_NOT_TOUCHABLE
+
+
+            // 调整悬浮窗显示的停靠位置为左侧置顶
+            wmParams.gravity = Gravity.LEFT | Gravity.TOP;
+            // 以屏幕左上角为原点，设置x、y初始值
+            wmParams.x = DensityUtil.px2dip(this, 0);
+            wmParams.y = DensityUtil.px2dip(this, 0);
+//		wmParams.x = DensityUtil.px2dip(this, 1200 -10);
+//		wmParams.y = DensityUtil.px2dip(this, 80);wmParams
+
+            // 设置悬浮窗口长宽数据
+            wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+            wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+            // 获取浮动窗口视图所在布局
+            mFloatLayout = (LinearLayout) LayoutInflater.from(getApplication()).inflate(R.layout.flashscreen_activity,
+                    null);
+
+            // 添加mFloatLayout
+            mWindowManager.addView(mFloatLayout, wmParams);
+
+            Log.i(TAG, "mFloatLayout-->left" + mFloatLayout.getLeft());
+            Log.i(TAG, "mFloatLayout-->right" + mFloatLayout.getRight());
+            Log.i(TAG, "mFloatLayout-->top" + mFloatLayout.getTop());
+            Log.i(TAG, "mFloatLayout-->bottom" + mFloatLayout.getBottom());
+
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+}
