f3f4ff8f26796e9cb098ba91b09076de5dfd9e6d
635488410@qq.com <635488410@qq.com>
2018-08-10 09:43:56 +0800

    111

--- app/src/main/java/phone/ktv/tootls/LoginRequestUtils.java
+++ app/src/main/java/phone/ktv/tootls/LoginRequestUtils.java
@@ -1,15 +1,12 @@
 package phone.ktv.tootls;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.google.gson.reflect.TypeToken;
 
-import java.io.IOException;
 import java.util.WeakHashMap;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 import phone.ktv.app.App;
 import phone.ktv.bean.AJson;
 import phone.ktv.bean.UserBean;
@@ -21,19 +18,14 @@ public class LoginRequestUtils {
 
     private static final String TAG = "LoginRequestUtils";
 
-    private static int RequestIndex = 10;
-
     private SPUtil mSP;
     private Context context;
+    private DefaultLoginInterface loginInterface;
 
-
-    public LoginRequestUtils(final SPUtil mSP, Context context) {
+    public LoginRequestUtils(final SPUtil mSP, Context context, DefaultLoginInterface loginInterface) {
         this.mSP = mSP;
         this.context = context;
-    }
-
-    public int getRequestIndex() {
-        return RequestIndex;
+        this.loginInterface = loginInterface;
     }
 
     public void requestLoginData() {
@@ -44,37 +36,60 @@ public class LoginRequestUtils {
         weakHashMap.put("pass", psd);//密码
         String url = App.getRqstUrl(App.headurl + "login", weakHashMap);
         Logger.i(TAG, "url.." + url);
-
         if (NetUtils.hasNetwork(context)) {
-            OkhttpUtils.doStart(url, new Callback() {
+            CallBackUtils.getInstance().init(url, new CallBackUtils.CommonCallback() {
                 @Override
-                public void onFailure(Call call, IOException e) {
-                    //返回失败
-                    RequestIndex = 20;
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    String s = response.body().string();
-                    Logger.i(TAG, "登录s.." + s);
-                    AJson aJson = App.jsonToObject(s, new TypeToken<AJson<UserBean>>() {
-                    });
-                    if (aJson != null) {
-                        if (aJson.getCode() == 0) {
-                            String token = aJson.getToken();
-                            mSP.putString("token", token);
-                            RequestIndex = 10;
-                        } else {
-                            RequestIndex = 20;
+                public void onFinish(String result, String msg) {
+                    if (TextUtils.isEmpty(result)) {
+                        loginInterface.resuleMessage(30);
+                    } else {
+                        AJson aJson = App.jsonToObject(result, new TypeToken<AJson<UserBean>>() {
+                        });
+                        if (aJson != null) {
+                            if (aJson.getCode() == 0) {
+                                String token = aJson.getToken();
+                                mSP.putString("token", token);
+                                loginInterface.resuleMessage(10);
+                            } else {
+                                loginInterface.resuleMessage(20);
+                            }
                         }
-                    }
-                    if (response.body() != null) {
-                        response.body().close();
                     }
                 }
             });
         } else {
-            ToastUtils.showShortToast(context, "网络连接异常,请检查网络配置");
+            ToastUtils.showLongToast(context, "网络连接异常,请检查网络配置");
         }
     }
+
+    public interface DefaultLoginInterface {
+        void resuleMessage(int code);
+    }
+
+    //用法:
+//                  case RankingExpiredToken:
+//            ToastUtils.showLongToast(mContext, (String) msg.obj);
+//    LoginRequestUtils utils = new LoginRequestUtils(mSP, mContext, loginInterface);
+//                    utils.requestLoginData();
+//                    break;
+//}
+//            mSvProgressHUD.dismiss();
+//                    mPullToRefresh.onRefreshComplete();
+//                    }
+//                    };
+//
+//                    LoginRequestUtils.DefaultLoginInterface loginInterface = new LoginRequestUtils.DefaultLoginInterface() {
+//@Override
+//public void resuleMessage(int code) {
+//        Logger.i(TAG, "code.." + code);
+//        switch (code) {
+//        case 10:
+//        break;
+//        case 20:
+//        break;
+//        case 30:
+//        break;
+//        }
+//        }
+//        };
 }
