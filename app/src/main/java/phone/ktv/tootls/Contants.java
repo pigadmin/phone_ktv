7ccec4d5e7390657aef0dd19f825100a98ff75eb
635488410@qq.com <635488410@qq.com>
2018-08-10 09:52:25 +0800

    111

--- app/src/main/java/phone/ktv/tootls/Contants.java
+++ app/src/main/java/phone/ktv/tootls/Contants.java
@@ -2,4 +2,15 @@ package phone.ktv.tootls;
 
 public class Contants {
     public static final int PermissRequest = 0x11;//6.0权限
+
+
+    public static class RequestInterfaceClass {
+        public static final int RequestSuccess = 100;//请求成功
+        public static final int RequestError = 200;//请求失败
+        public static final int RequestExpiredToken = 300;//token失效
+        public static final int ServerException = 400;//服务器异常
+        public static final int DataParsingException = 500;//数据解析异常
+        public static final int NoNetworkPresent = 600;//当前无网络
+        public static final int OtherError = 700;//其他错误
+    }
 }
