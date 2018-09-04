7f035f0f456b8a7d8fa8257f7429eec6638f0134
635488410@qq.com <635488410@qq.com>
2018-09-04 18:15:58 +0800

    11

--- app/src/main/java/phone/ktv/tootls/NetUtils.java
+++ app/src/main/java/phone/ktv/tootls/NetUtils.java
@@ -6,25 +6,25 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
-/**
- * Created by yz1309 on 2015-07-30.
- */
+
 public class NetUtils {
     /**
      * 得到Mac地址
+     *
      * @param context
      * @return
      */
-    public static String getMac(Context context){
+    public static String getMac(Context context) {
         String res = "未打开Wifi";
         WifiManager wifi = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
         WifiInfo info = wifi.getConnectionInfo();
         if (info.getMacAddress() != null)
-            res =info.getMacAddress();
+            res = info.getMacAddress();
 
         return res;
     }
 
+    // 判断网络连接状态
     public static boolean hasNetwork(Context context) {
         ConnectivityManager con = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo workinfo = con.getActiveNetworkInfo();
@@ -34,4 +34,45 @@ public class NetUtils {
         return true;
     }
 
+    // 判断wifi状态
+    public static boolean isWifiConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mWiFiNetworkInfo = mConnectivityManager
+                    .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (mWiFiNetworkInfo != null) {
+                return mWiFiNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    // 判断移动网络
+    public static boolean isMobileConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mMobileNetworkInfo = mConnectivityManager
+                    .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if (mMobileNetworkInfo != null) {
+                return mMobileNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    // 获取连接类型
+    public static int getConnectedType(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager
+                    .getActiveNetworkInfo();
+            if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
+                return mNetworkInfo.getType();
+            }
+        }
+        return -1;
+    }
 }
